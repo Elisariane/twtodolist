1753046beac0e196bdbc2558e0cdb7d6d649eea8
Elisariane <elisarianeBS@gmail.com>
2023-12-28 10:45:54 -0300

    :sparkles: feat: Página de cadastro

--- todos/models.py
+++ todos/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null= False, blank=False)
-    created_at = models.DateField(auto_now_add=True, null= False, blank=False)
-    deadline = models.DateField(null= False, blank=False)
-    finished_at = models.DateField(null= True)
+    title = models.CharField(
+        verbose_name="Título", max_length=100, null=False, blank=False
+    )
+    created_at = models.DateField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateField(verbose_name="Data de entrega", null=False, blank=False)
+    finished_at = models.DateField(null=True)
