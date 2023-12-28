559ab0caaf3b73c97bbb8acd0a6c74d213a9b355
Elisariane <elisarianeBS@gmail.com>
2023-12-28 11:40:55 -0300

    :sparkles: feat: Função de conclusão de tarefa

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
 from .models import Todo
+from django.shortcuts import get_object_or_404, redirect
 
 
 class TodoListView(ListView):
@@ -12,11 +13,20 @@ class TodoCreateView(CreateView):
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
 
+
 class TodoUpdateView(UpdateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
 
+
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
